e120edd636b7abce861b1843f08ff41f8c1c5641
Michael Osborne <resonatortune@gmail.com>
2019-07-05 13:31:06 -0700

    Add Output Management example with additions

---
+++ webpack-demos/Output Management/dev/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+
+module.exports = {
+
+  // FOR DEVELOPMENT
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../')
+  },
+
+
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Output Management'
+    }),
+
+    // cleanup everything thats not related to development in the root directory for
+    // deploying to github pages. (static files must be in root directory for gh pages)
+    // new WebpackCleanupPlugin({
+    //   exclude: [path.join(__dirname, '../dev/')],
+    //   preview: true,
+    // }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, '../')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, ]
+  },
+};
